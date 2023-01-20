ecc498f85e711731faed4c54a872e9e9dad33915
Partha P. Das <parthopdas@live.com>
2023-01-19 19:36:26 -0500

    enhancing incompatible extensions detecting logic

--- src/RustAnalyzer/RustAnalyzerPackage.cs
+++ src/RustAnalyzer/RustAnalyzerPackage.cs
@@ -10,6 +10,7 @@ using System.Threading.Tasks;
 using Community.VisualStudio.Toolkit;
 using KS.RustAnalyzer.Common;
 using KS.RustAnalyzer.VS;
+using Microsoft.NET.StringTools;
 using Microsoft.VisualStudio;
 using Microsoft.VisualStudio.ComponentModelHost;
 using Microsoft.VisualStudio.Shell;
@@ -57,27 +58,27 @@ public sealed class RustAnalyzerPackage : ToolkitPackage
             var exMgrType = exMgrAssembly.GetType("Microsoft.VisualStudio.ExtensionManager.SVsExtensionManager");
             dynamic exMgr = GetGlobalService(exMgrType);
 
-            var enabledExtensions = exMgr.GetEnabledExtensions() as IEnumerable<dynamic>;
-            var incompatibleExtensions = enabledExtensions
-                .Where(x => (x.Header.Name as string).Equals("VS_RustAnalyzer", StringComparison.OrdinalIgnoreCase));
-            if (incompatibleExtensions.Any())
+            var allExtensions = exMgr.GetInstalledExtensions() as IEnumerable<dynamic>;
+            var allExtensionIds = allExtensions.ToDictionary(x => x.Identifier as string);
+            var incompatibleExtensions = AreIncompatibleExtensionsInstalled(allExtensionIds);
+            if (incompatibleExtensions.Count != 0)
             {
                 var mbRet = await new MessageBox()
                     .ShowAsync(
-                        $"{Vsix.Name} has detected incompatible extensions",
-                        "Disable them and restart Visual Studio? You can enable them back later from Extensions > Manage Extensions.");
+                        $"{Vsix.Name} has detected the followiing incompatible extensions:\r\n\r\n{string.Join("\r\n", incompatibleExtensions.Select(x => x.Id))}",
+                        $"- OK: Disable the above and restart VS. (You can enable them back later from Extensions > Manage Extensions.)\r\n- Cancel: Disable {Vsix.Name} and restart VS.");
                 if (mbRet == VSConstants.MessageBoxResult.IDOK)
                 {
                     _telemetry?.TrackEvent("DisableIncompatExts", ("NumberOfExts", incompatibleExtensions.Count().ToString(CultureInfo.InvariantCulture)));
                     foreach (var e in incompatibleExtensions)
                     {
-                        exMgr.Disable(e);
+                        exMgr.Disable(e.Extension);
                     }
                 }
                 else
                 {
                     _telemetry?.TrackEvent("DisableThisExt");
-                    var thisExtension = enabledExtensions.Where(e => (e.Header.Name as string).Equals(Vsix.Name, StringComparison.OrdinalIgnoreCase));
+                    var thisExtension = allExtensionIds[Vsix.Id];
                     exMgr.Disable(thisExtension);
                 }
 
@@ -96,9 +97,32 @@ public sealed class RustAnalyzerPackage : ToolkitPackage
 
         var args = Environment.GetCommandLineArgs();
         Process.Start(args[0], string.Join(" ", args.Skip(1)));
-
         await Task.Delay(2000);
 
         (GetGlobalService(typeof(EnvDTE.DTE)) as EnvDTE.DTE).Quit();
+    }
+
+    private static IReadOnlyList<(string Id, dynamic Extension)> AreIncompatibleExtensionsInstalled(IDictionary<string, dynamic> allExtensions)
+    {
+        var incompatibleExtensions = new[]
+        {
+            "SourceGear.Rust.0c9f177a-b25e-4f25-9a35-b9049b4f9c9c",
+            "VS_RustAnalyzer.c5a2b628-2a68-4643-808e-0838e3fb240b",
+        };
+
+        var installedIncompatibleExtensions = incompatibleExtensions
+            .Aggregate(
+                new List<(string, dynamic)>(),
+                (acc, e) =>
+                {
+                    if (allExtensions.ContainsKey(e) && allExtensions[e].State.ToString() != "Disabled")
+                    {
+                        acc.Add((e, allExtensions[e]));
+                    }
+
+                    return acc;
+                });
+
+        return installedIncompatibleExtensions;
     }
 }
