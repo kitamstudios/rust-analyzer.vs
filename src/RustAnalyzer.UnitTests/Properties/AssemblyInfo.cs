b55b7de7b3b7e34181203b8856291ce6ceac545f
Partha P. Das <parthopdas@live.com>
2023-01-23 07:50:33 -0500

    Fixing CDP tests

--- src/RustAnalyzer.UnitTests/Properties/AssemblyInfo.cs
+++ src/RustAnalyzer.UnitTests/Properties/AssemblyInfo.cs
@@ -35,4 +35,4 @@ using ApprovalTests.Reporters;
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
 
-[assembly: IgnoreLineEndings(true)]
+[assembly: IgnoreLineEndings(false)]
