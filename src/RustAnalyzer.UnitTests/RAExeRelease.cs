c9a01adb52dfad35fced5b27f63e4e61db5f42e9
Partha P. Das <parthopdas@live.com>
2023-12-18 11:33:41 -0400

    Updated to RLS 2023-12-18 & VS 17.8

--- src/RustAnalyzer.UnitTests/RAExeRelease.cs
+++ src/RustAnalyzer.UnitTests/RAExeRelease.cs
@@ -12,7 +12,7 @@ namespace KS.RustAnalyzer.UnitTests;
 
 public sealed class RAExeRelease
 {
-    private const string LastUpdatedRAExeVersion = "2023-11-06";
+    private const string LastUpdatedRAExeVersion = "2023-12-18";
 
     [Fact]
     public async Task LastUpdateShouldNotBeOlderThan30DaysAsync()
