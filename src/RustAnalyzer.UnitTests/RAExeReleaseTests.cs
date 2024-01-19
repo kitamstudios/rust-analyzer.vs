b3d2f1b4ffb9237318cf7d9c199bb415e24b313e
Partha P. Das <parthopdas@live.com>
2024-01-19 01:00:44 -0500

    Autodownload RA under feature flag.

--- src/RustAnalyzer.UnitTests/RAExeReleaseTests.cs
+++ src/RustAnalyzer.UnitTests/RAExeReleaseTests.cs
@@ -16,6 +16,6 @@ public sealed class RAExeReleaseTests
 
         var latestRelDate = DateTime.ParseExact(ret?.Version, RADownloaderService.RAVersionFormat, CultureInfo.InvariantCulture);
         var lastUpdateDate = DateTime.ParseExact(RADownloaderService.LatestInPackageRAVersion, RADownloaderService.RAVersionFormat, CultureInfo.InvariantCulture);
-        lastUpdateDate.Should().NotBeBefore(latestRelDate.AddDays(-30), $"new rust-analyzer.exe is available https://github.com/rust-lang/rust-analyzer/releases/download/{ret?.Version}/rust-analyzer-x86_64-pc-windows-msvc.zip");
+        lastUpdateDate.Should().NotBeBefore(latestRelDate.AddDays(-120), $"new rust-analyzer.exe is available {ret?.Uri}");
     }
 }
--- src/RustAnalyzer/Infrastructure/RADownloaderService.cs
+++ src/RustAnalyzer/Infrastructure/RADownloaderService.cs
@@ -2,12 +2,15 @@ using System;
 using System.ComponentModel.Composition;
 using System.Globalization;
 using System.IO;
+using System.IO.Compression;
 using System.Linq;
 using System.Net.Http;
 using System.Reflection;
 using System.Threading;
 using System.Threading.Tasks;
 using KS.RustAnalyzer.TestAdapter.Common;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.Win32;
 
 namespace KS.RustAnalyzer.Infrastructure;
 
@@ -23,8 +26,8 @@ public interface IRADownloaderService
 public class RADownloaderService : IRADownloaderService
 {
     public const string LatestInPackageRAVersion = "2024-01-08";
-
     public const string RAVersionFormat = "yyyy-MM-dd";
+    private const string InstalledRAVersionKey = "InstalledRAVersion";
     private readonly IRegistrySettingsService _regSettings;
     private readonly TL _tl;
 
@@ -39,16 +42,44 @@ public class RADownloaderService : IRADownloaderService
         };
     }
 
-    public Task DownloadLatestRAAsync()
+    public async Task DownloadLatestRAAsync()
     {
+        if (!IsDownloadEnabled())
+        {
+            return;
+        }
+
         _tl.L.WriteLine("Initiating download of RA...");
-        return Task.CompletedTask;
+        try
+        {
+            var latestRel = await GetLatestRAReleaseRedirectUriAsync();
+            string installedVer = await GetInstalledVersionAsync();
+            if (latestRel != null && installedVer.CompareTo(latestRel?.Version) >= 0)
+            {
+                _tl.L.WriteLine($"Not going to download RA. Installed = {installedVer}, Latest = {latestRel?.Uri}.");
+                _tl.T.TrackEvent("RADS.RAUpToDate", ("Installed", installedVer), ("Latest", latestRel?.Uri.ToString()));
+                return;
+            }
+
+            using var response = await DownloadAsync(latestRel);
+
+            using var zipStream = await response.Content.ReadAsStreamAsync();
+            Install(zipStream, latestRel?.Version);
+
+            await CommitAsync(latestRel);
+            _tl.T.TrackEvent("RADS.RAInstalled", ("Installed", installedVer));
+        }
+        catch (Exception ex)
+        {
+            _tl.L.WriteError($"Download failed. StatusCode {ex}");
+            _tl.T.TrackException(ex);
+            throw;
+        }
     }
 
-    public Task<PathEx> GetRustAnalyzerExePathAsync()
+    public async Task<PathEx> GetRustAnalyzerExePathAsync()
     {
-        var path = (PathEx)Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), $"rust-analyzer.{LatestInPackageRAVersion}.exe");
-        return path.ToTask();
+        return GetVersionedRAExePath(await GetInstalledVersionAsync());
     }
 
     public static async Task<(Uri Uri, string Version)?> GetLatestRAReleaseRedirectUriAsync()
@@ -60,12 +91,89 @@ public class RADownloaderService : IRADownloaderService
             var latestRelVersion = latestRelUri.Segments[latestRelUri.Segments.Length - 1];
             var latestRelDate = DateTime.ParseExact(latestRelVersion, RAVersionFormat, CultureInfo.InvariantCulture);
 
-            return (Uri: latestRelUri, Version: latestRelDate.ToString(RAVersionFormat, CultureInfo.InvariantCulture));
+            return (Uri: new Uri($"https://github.com/rust-lang/rust-analyzer/releases/download/{latestRelVersion}/rust-analyzer-x86_64-pc-windows-msvc.zip"),
+                Version: latestRelDate.ToString(RAVersionFormat, CultureInfo.InvariantCulture));
         }
         catch
         {
             return null;
         }
+    }
+
+    private bool IsDownloadEnabled()
+    {
+        var id = Environment.ExpandEnvironmentVariables("%USERNAME%@%COMPUTERNAME%.%USERDOMAIN%");
+        return id?.Equals("parth@unrenormlizable.unrenormlizable", StringComparison.OrdinalIgnoreCase) ?? false;
+    }
+
+    private PathEx GetVersionedRAExePath(string version)
+    {
+        return GetRAFolder(version) + (PathEx)$"rust-analyzer.exe";
+    }
+
+    private async Task<HttpResponseMessage> DownloadAsync((Uri Uri, string Version)? latestRel)
+    {
+        _tl.L.WriteLine($"Downloading RA v{latestRel?.Uri}.");
+        var response = await new HttpClient().GetAsync(latestRel?.Uri);
+        if (!response.IsSuccessStatusCode)
+        {
+            _tl.L.WriteError($"Download failed. StatusCode {response.StatusCode}.");
+            _tl.T.TrackEvent("RADS.RADownloadFailed", ("StatusCode", response.StatusCode.ToString()));
+            throw new Exception($"RADS.RADownloadFailed. {response.StatusCode}.");
+        }
+
+        return response;
+    }
+
+    private async Task CommitAsync((Uri Uri, string Version)? latestRel)
+    {
+        await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync();
+        if (!_regSettings.GetPackageRegistryRoot(out var regRoot))
+        {
+            _tl.L.WriteError($"GetPackageRegistryRoot failed.");
+            throw new Exception($"GetPackageRegistryRoot failed.");
+        }
+
+        Registry.SetValue(regRoot, InstalledRAVersionKey, latestRel?.Version);
+        _tl.L.WriteLine($"Committed RA installation.");
+    }
+
+    private void Install(Stream zipStream, string downloadedVersion)
+    {
+        _tl.L.WriteLine($"Installing RA v{downloadedVersion}...");
+        var raFolder = GetRAFolder(downloadedVersion);
+        Directory.CreateDirectory(raFolder);
+        using var zip = new ZipArchive(zipStream);
+        foreach (var entry in zip.Entries)
+        {
+            var dstFile = raFolder + entry.FullName;
+            entry.ExtractToFile(dstFile, true);
+            _tl.L.WriteLine($"... Installing {dstFile}");
+        }
+    }
+
+    private async Task<string> GetInstalledVersionAsync()
+    {
+        await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync();
+
+        var installedRAVersion = LatestInPackageRAVersion;
+        if (_regSettings.GetPackageRegistryRoot(out var regRoot))
+        {
+            installedRAVersion = Registry.GetValue(regRoot, InstalledRAVersionKey, null) as string;
+        }
+
+        installedRAVersion ??= LatestInPackageRAVersion;
+        if (GetVersionedRAExePath(installedRAVersion).FileExists())
+        {
+            return installedRAVersion;
+        }
+
+        return LatestInPackageRAVersion;
+    }
+
+    private static PathEx GetRAFolder(string version)
+    {
+        return (PathEx)Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + (PathEx)version;
     }
 
     private static async Task<Uri> GetRedirectedUrlAsync(Uri uri, CancellationToken cancellationToken = default)
