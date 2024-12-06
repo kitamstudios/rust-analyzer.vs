47a22abc949fece85766fe94841398a68780ab49
unrenormalizable <unrenormalizable@gmail.com>
2024-12-06 13:34:03 +0530

    r-a.exe 2024-12-02

--- src/RustAnalyzer.TestAdapter/Constants.cs
+++ src/RustAnalyzer.TestAdapter/Constants.cs
@@ -31,7 +31,7 @@ public static class Constants
     public const string TestsContainerExtension = ".rusttests";
     public const string TestContainersSearchPattern = $"*{TestsContainerExtension}";
 
-    public const string RlsLatestInPackageVersion = "2024-11-25";
+    public const string RlsLatestInPackageVersion = "2024-12-02";
     public static readonly Version MinimumRequiredVsVersion = new(17, 12);
 
     public static readonly PathEx TestsContainerExtension2 = (PathEx)TestsContainerExtension;
